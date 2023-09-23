2d4ec84f14f6430510f8120dcf5332450f5ad114
Matheus Souza <matheusholly1@gmail.com>
2023-09-22 23:47:54 -0300

    commit: scrollsauve.js

--- assets/js/scrollsauve.js
+++ assets/js/scrollsauve.js
@@ -10,6 +10,3 @@ sr.reveal('.icons-front-end', { duration: 3000, origin: 'left' });
 sr.reveal('.txt-sobre-mim', { duration: 3000, origin: 'right'});
 sr.reveal('.flex-3', { duration: 3000, origin: 'bottom' });
 sr.reveal('.flex-4', { duration: 3000, origin: 'bottom' });
-sr.reveal('.github-link', { duration: 3000, origin: 'bottom' });
-sr.reveal('.conteiner-skills', { duration: 3000, origin: 'left' });
-sr.reveal('.conteiner-descricao', { duration: 3000, origin: 'right' });
